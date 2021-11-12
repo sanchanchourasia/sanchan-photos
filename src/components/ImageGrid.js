e44cf169a1d4576822b48af07e13c8f494003244
Sanchan Chourasia <sanchanchourasia.ips@gmail.com>
2021-11-12 15:00:14 +0530

    3

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import useFirestore from '../hooks/useFirestore'
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
 
 const ImageGrid = () => {
-    const { docs } = useFirestore('images');
-    console.log(docs);
+  const { docs } = useFirestore("images");
+  console.log(docs);
 
-    return (
-        <div className="img-grid">
-           { docs && docs.map(doc => (
-               <div className='img-wrap' key={doc.id}>
-                   <img src={doc.url} alt='pic not available' style={{width:'100%', height:'100%'}} /> </div>
-           ))}           
-        </div>
-    )
-}
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div className="img-wrap" key={doc.id}>
+            <img
+              src={doc.url}
+              alt="pic not available"
+              style={{ width: "100%", height: "100%" }}
+            />{" "}
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default ImageGrid;
