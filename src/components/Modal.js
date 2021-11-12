3f7bdd24c8535912b22cd9d06170c1afe46fa863
Sanchan Chourasia <sanchanchourasia.ips@gmail.com>
2021-11-12 15:56:49 +0530

    3

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
-    const handleClick = (e) => {
-        setSelectedImg(null);
-    }
+  const handleClick = (e) => {
+    if (e.target.classList.contains('backdrop')) 
+      setSelectedImg(null);
+    
+  };
   return (
     <div className="backdrop" onClick={handleClick}>
       <img src={selectedImg} alt="zoom-img" />
-    </div> 
+    </div>
   );
 };
 
