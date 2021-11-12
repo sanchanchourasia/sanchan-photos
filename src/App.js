d3662dff6473a0b5c99bc0b2bdaa5575f37dc678
Sanchan Chourasia <sanchanchourasia.ips@gmail.com>
2021-11-12 15:53:04 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Title from './components/Title';
 import { UploadForm } from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null)
+
   return (
     <div>
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      { selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} /> }
       
     </div>
   );
---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Modal = ({ selectedImg, setSelectedImg }) => {
+    const handleClick = (e) => {
+        setSelectedImg(null);
+    }
+  return (
+    <div className="backdrop" onClick={handleClick}>
+      <img src={selectedImg} alt="zoom-img" />
+    </div> 
+  );
+};
+
+export default Modal;
