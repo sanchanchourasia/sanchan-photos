6beef6264c90440bf577aa67e6b25f0bb8336b62
Sanchan Chourasia <sanchanchourasia.ips@gmail.com>
2021-11-18 15:15:17 +0530

    5

--- src/components/Title.js
+++ src/components/Title.js
@@ -12,7 +12,7 @@ const Title = () => {
         style={{ height: "30px", marginTop: "10px" }}
       />{" "}
       <h1>Sanchan's Photo Gallery</h1>
-      <p>This website is photos Gallery....</p>
+      <p>Somewhere between emotion and emotionless, i grew up....</p>
       <h2>My Pictures!</h2>
     </div>
   );
